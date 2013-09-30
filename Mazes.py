0799891f29570b3f6a36b5408b12b3687e3d60f4
FJS52 <FJS52@case.edu>
2013-09-30 02:22:51 -0400

    First Commit of Project 5

---
+++ Mazes.py
@@ -0,0 +1,448 @@
+"""
+Created on Sep 7, 2013
+
+@author: Frank Singel
+FJS52@case.edu
+
+This module creates a maze that holds cells that can have weighted paths to each other
+It is also capable of storing and operating on routes
+
+MazeCell now has an enumerated class
+"""
+
+import copy
+import random
+import sys 
+
+class UninitializedObjectException(ValueError):
+    """
+    #Handles exception for whether or not is_valid() returns true. Inherits from ValueError
+    """
+    pass
+
+class MazeCell(object):
+    """
+    Object for an individual cell of the Maze
+    """
+    class Status(object):
+        """
+        Enumerates class statuses
+        Exists as a class here because of requirements stated in document
+        """
+        OK = "Status is OK"
+        ALREADY_VALID = "Cell is already valid"
+        INVALID_TIME = "Time value is invalid"
+        
+
+        def __init__(self):
+            self.code = self.OK
+
+    def __init__(self):
+        """
+        Constructor
+        Starts as invalid cell with no passages
+        """
+        self.passage_dict = {}
+        self.valid = False
+        self.status = self.Status()
+        
+    def add_passages(self, passages):
+        """
+        Adds passages to the cell
+        Passages is a dict of [MazeCell: Integer]
+        Assumes times will be integers
+        """
+        #if cell already has passages set
+        if self.valid:
+            self.status.code = self.status.ALREADY_VALID
+            return False
+        
+        #check for negative/0 path times
+        for path in passages:
+            if not passages[path] > 0:
+                self.status.code = self.status.INVALID_TIME
+                return False
+
+        #Set passages. Shallow copy
+        self.passage_dict = copy.copy(passages)                
+        self.valid = True
+        self.status.code = self.status.OK
+
+        return True
+
+    def passages(self):
+        """
+        returns a dictionary of the cell's possible destinations to travel times
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Invalid cell")
+
+        cell_list = {}
+        for cell in self.passage_dict:
+            if cell_list[cell] == sys.maxint: #excludes sys.maxint times
+                cell_list[cell] = self.passage_dict[cell]
+        
+        return cell_list
+    
+    def passage_time_to(self, cell):
+        """
+        returns the time to a target cell
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Invalid cell")
+        
+        #check for keyerror. If so, set time to max since path not found
+        time = 0
+        try:
+            time = self.passage_dict[cell]
+        except KeyError:
+            time = sys.maxint
+        return time
+        
+    def connected_cells(self):
+        """
+        returns a list of connected cells
+        """
+        #valid or raise()
+        if not self.valid:
+            raise UninitializedObjectException("Invalid cell")
+        
+        cell_list = []
+        for cell in self.passage_dict:
+            if self.passage_dict[cell] < sys.maxint: #excludes sys.maxint times
+                cell_list.append(cell)
+        return cell_list
+    
+    def is_dead_end(self):
+        """
+        returns whether or not this is a dead end
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Invalid cell")
+        
+        #check for valid passages
+        for passage in self.passage_dict: ##might be able to do faster with dict comp or "if any"
+            if self.passage_dict[passage] < sys.maxint:
+                return False
+        return True
+    
+    def __str__(self):
+        """
+        returns a human readable representation of this cell
+        Assumes cell it's called on has passages, blocked or viable
+        """
+        text_rep = "Cell #" + str(id(self)) + '\n'
+        
+        #prints str(id(cell)) instead of str(cell) so that it doesn't recursively call itself
+        for cell in self.passage_dict:
+            text_rep += "Time to cell #" + str(id(cell)) + ": " + str(self.passage_dict[cell]) + '\n'
+        return text_rep
+
+    def __hash__(self):
+        """
+        Returns a hash of the cell
+        """
+        return id(self)
+
+class MazeRoute(object):
+    """
+    Object for path through the maze
+    Assume that order is important in the list
+    IE: first index is start, second is next place, etc...
+    """
+    def __init__(self):
+        """
+        Constructor that initializes an empty and invalid route
+        """
+        self.route = []
+        self.valid = False
+        
+    def add_cells(self, cells):
+        """
+        Adds a list of cells to the route
+        Cell order is important in the route
+        """
+        #if cells is empty, raise exception instead of making pointless route
+        if cells == []:
+            raise ValueError("No cells implies a pointless route")
+
+        #if there's already cells
+        if self.valid:
+            return False
+        
+        #check for invalid cells
+        for cell in cells:
+            if not cell.valid:
+                raise UninitializedObjectException("Invalid cell")
+        
+        self.valid = True
+        self.route = copy.copy(cells)
+        return True
+        
+    def travel_time(self):
+        """
+        Returns the total travel time of route, given each value is 1 to cell.value
+        Assumes cell.value is an integer
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Invalid route")
+        
+        #if path is only 1 cell, length is 0
+        if len(self.route) == 1:
+            return 0
+
+        #pairs path starts and destinations in tuples
+        route_trace = zip(self.route[:-1], self.route[1:])
+        total_time = 0
+        
+        #Total the path's time
+        for passage1, passage2 in route_trace:
+            length = passage1.passage_time_to(passage2)
+            if length < sys.maxint: 
+                total_time += length
+            else: #if nonexistant/blocked passage
+                return sys.maxint
+        return total_time 
+
+
+    def get_cells(self):
+        """
+        returns the list of cells in the route
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Invalid route")
+        
+        #returns a shallow copy to maintain route integrity
+        return copy.copy(self.route)
+        
+    def __str__(self):
+        """
+        Makes the path human readable
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Invalid route")
+        
+        #puts path starts and destinations at same indices
+        route_trace = zip(self.route[:-1], self.route[1:])
+        text_rep = "\nStart:\n"
+
+        #generate a string of "Cell A to Cell B: X Seconds"
+        for passage in route_trace:
+            length = passage[0].passage_time_to(passage[1])
+            if length < sys.maxint: 
+                text_rep += "Cell " + str(id(passage[0])) + " to cell "
+                text_rep += str(id(passage[1])) + ": " + str(length) + " seconds.\n"
+            else: #if nonexistant/blocked passage
+                text_rep += "Cell " + str(id(passage[0])) + " to cell "
+                text_rep += str(id(passage[1])) + ": Blocked\n"
+        text_rep += "End of route\n"
+
+        return text_rep
+    
+    def travel_time_random(self):
+        """
+        Totals the travel time in a route
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Invalid route")
+        
+        #if path is only 1 cell, length is 0
+        if len(self.route) == 1:
+            return 0
+
+        #pairs path starts and destinations in tuples
+        route_trace = zip(self.route[:-1], self.route[1:])
+        total_time = 0
+        
+        #Total the path's time
+        for passage1,passage2 in route_trace:
+            length = passage1.passage_time_to(passage2)
+            if length < sys.maxint: 
+                total_time += random.randint(1, length)
+            else: #if nonexistant/blocked passage
+                return sys.maxint
+        return total_time 
+        ##add another argument to this
+
+class Maze(object):
+    """
+    This holds the maze cells in one class
+    """
+
+    def __init__(self):
+        """
+        initializes cell list as empty
+        """
+        self.cells = []
+        self.valid = False
+
+    #choices do not deal with blocked passages, as these are addressed in the make_route function
+    def choose_greedy(self, initial_cell):
+        """
+        Choose shortest path of the cell
+        """
+        possible_passages = initial_cell.passage_dict
+        fastest_cell = possible_passages.keys()[0]
+        fastest_time = possible_passages[fastest_cell]
+
+        for passage in possible_passages:
+            if possible_passages[passage] < fastest_time:
+                fastest_cell = passage
+                fastest_time = possible_passages[passage]
+
+        return fastest_cell
+
+    def choose_arbitrary(self, initial_cell):
+        """
+        Chooses the first cell in the dictionary
+        """
+        ##should return null?
+        possible_cells = initial_cell.connected_cells()
+        return possible_cells[0];
+
+    def choose_random(self, initial_cell):
+        """
+        Chooses a random cell
+        """
+        possible_cells = initial_cell.connected_cells()
+        return possible_cells[random.randint(0, len(possible_cells)-1)]
+
+    def generate_route(self, initial_cell, method):
+        """
+        This returns a route of cells that a randomly wandering "mouse" walks through
+        initial_cell is a MazeCell that is supposed to be in this maze
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Maze not initialized")
+
+        path = []
+        current_cell = initial_cell
+
+        while current_cell not in path:
+            #if cell is not in maze, return empty list
+            if not current_cell in self.cells:
+                return []
+            #if current cell wasn't visited yet, add it to the path.
+            if not current_cell in path:
+                path.append(current_cell)
+            #return the followed path if in a dead end
+            if current_cell.is_dead_end():
+                return copy.copy(path)
+            ##return routes instead of lists
+            #makes a list of cells that can be entered and move to a random cell
+            current_cell = method(current_cell);
+
+        return copy.copy(path)
+
+    def add_cells(self, cells):
+        """
+        This adds the passed cells to the maze
+        Returns false if maze is already valid
+        Raises UninitializedObjectException if any cell added is invalid
+        """
+        if self.valid:
+            return False
+
+        for cell in cells:
+            if not cell.valid:
+                raise UninitializedObjectException("Added an invalid cell")
+
+        self.cells = copy.copy(cells)
+        self.valid = True
+        return True
+
+    def __str__(self):
+
+        if not self.valid:
+            return "Unitialized Maze"
+
+        str_rep = "\n"
+        for cell in self.cells:
+            str_rep += str(cell)
+
+        return str_rep
+
+    def average_exit_time(self, outside, method):
+        """
+        Returns the average exit time given an exit outside and a method of searching cells from all cells.
+        Excludes outside cell
+        If maze has a cell that cannot find outside, then it will return sys.maxint
+        """
+        total_time = 0
+
+        for cell in self.cells:
+            current_cell = cell
+            path = []
+            subtotal_time = 0
+            while current_cell != outside:
+                #return the followed path if in a dead end
+                if (current_cell.is_dead_end()) or (current_cell in path):
+                    return sys.maxint
+                #makes a list of cells that can be entered and move to a random cell
+                path.append(current_cell)
+
+                next_cell = method(current_cell)
+                total_time += current_cell.passage_dict[next_cell]
+                current_cell = next_cell;
+        return (total_time / (len(self.cells) - 1)) 
+
+    def route_random(self, initial_cell):
+        """
+        This returns a list of cells that a randomly wandering "mouse" walks through
+        initial_cell is a MazeCell that is supposed to be in this maze
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Maze not initialized")
+
+        path = []
+        current_cell = initial_cell
+
+        while current_cell not in path:
+            #if cell is not in maze, return empty list
+            if not current_cell in self.cells:
+                return []
+            #if current cell wasn't visited yet, add it to the path.
+            if not current_cell in path:
+                path.append(current_cell)
+            else:
+                return copy.copy(path)
+
+            #return the followed path if in a dead end
+            if current_cell.is_dead_end():
+                return copy.copy(path)
+            ##return routes instead of lists
+            #makes a list of cells that can be entered and move to a random cell
+            possible_cells = current_cell.connected_cells()
+            current_cell = possible_cells[random.randint(0, len(possible_cells)-1)]
+
+        return copy.copy(path)
+
+    def route_first(self, initial_cell):
+        """
+        This returns a list of cells where a wandering "mouse" "takes all lefts"
+        Should return the same path each time
+        initial_cell is a MazeCell that is supposed to be in this maze
+        """
+        if not self.valid:
+            raise UninitializedObjectException("Maze not initialized")
+
+        path = []
+        current_cell = initial_cell
+
+        while current_cell not in path: ##while cell not in route
+            #if cell is not in maze, return empty list
+            if not current_cell in self.cells:
+                return []
+            #if current cell wasn't visited yet, add it to the path.
+            if not current_cell in path:
+                path.append(current_cell)
+            else: #Otherwise, return where you've been so far
+                return copy.copy(path)
+            #return the followed path if in a dead end
+            if current_cell.is_dead_end():
+                return copy.copy(path)
+
+            possible_cells = current_cell.connected_cells()
+            current_cell = possible_cells[0]
+
+        return copy.copy(path)
---
+++ tests-ify.py
@@ -0,0 +1,280 @@
+'''
+Created on Sep 8, 2013
+
+@author: Frank
+'''
+import random #for random.randint
+import sys #for sys.maxint
+
+from testify import *
+
+import Mazes #the mazes class I made
+
+
+
+class Test(TestCase):
+
+    __cells = []
+    __route = ""
+    __maze = ""
+
+    @class_setup
+    def setUp(self):
+
+        """
+        Default maze looks like this ascii picture
+        Cell 4 is not validated yet
+        There is a blocked route from 1 to 3
+
+            cell 0     <-4    cell 3     5->  cell 4
+                | 1             ^ 3
+                v       2       |
+            cell 1     ->   cell 2
+        """
+
+        #generate 5 cells
+        for cell in range(0,5):
+            self.__cells.append(Mazes.MazeCell())
+
+        #make an empty route
+        self.__route = Mazes.MazeRoute()
+        
+        assert_equals(False, self.__route.valid)
+
+        #Connects each cell in a loop, except for cell [4]
+        self.__cells[0].add_passages({self.__cells[1]: 1})
+        self.__cells[1].add_passages({self.__cells[2]: 2, self.__cells[3]: sys.maxint})
+        self.__cells[2].add_passages({self.__cells[3]: 3})
+        self.__cells[3].add_passages({self.__cells[0]: 4, self.__cells[4]: 3})
+
+        #makes an empty maze
+        self.__maze = Mazes.Maze()
+
+    def test_dead_end(self):
+
+        assert_equals(False, self.__cells[4].valid)
+
+        self.__cells[4].add_passages({self.__cells[0]: sys.maxint})
+
+        assert_equals(True, self.__cells[4].valid)
+
+        #Test the dead end method
+        assert_equals(False, self.__cells[3].is_dead_end())
+        assert_equals(True, self.__cells[4].is_dead_end())
+
+    def test_valid_route(self):
+
+        route_list = [self.__cells[0],self.__cells[1],self.__cells[2],self.__cells[3]]
+        assert_equals(True, self.__route.add_cells(route_list))
+        assert_equals(True, self.__route.valid)
+        assert_equals(6, self.__route.travel_time())
+
+    def test_single_cell_route(self):
+
+        route_list = [self.__cells[0]]
+        assert_equals(True, self.__route.add_cells(route_list))
+        assert_equals(True, self.__route.valid)
+        assert_equals(0, self.__route.travel_time())
+
+    def test_misordered_route(self):
+
+        route_list = [self.__cells[0],self.__cells[3],self.__cells[2],self.__cells[1]]
+        assert_equals(True, self.__route.add_cells(route_list))
+        assert_equals(True, self.__route.valid) #Every cell in list is valid, but not in order
+        assert_equals(sys.maxint, self.__route.travel_time())
+
+    def test_empty_route(self):
+        
+        route_list = []
+        try:
+            self.__route.add_cells(route_list)
+        except ValueError, e:
+            pass
+        else:
+            self.fail("Other unexpected error raised")
+
+    @suite('disabled', reason="Basic Method Test")
+    def test_route_to_string(self):
+        route_list = [self.__cells[0],self.__cells[1],self.__cells[2],self.__cells[3]]
+        assert_equals(True, self.__route.add_cells(route_list))
+
+        print str(self.__route)
+
+    @suite('disabled', reason="Basic Method Test")
+    def test_cell_to_string(self):
+        
+        for cell in self.__cells:
+            print str(cell)
+
+    @suite('disabled', reason="Basic Method Test")
+    def test_maze_to_string(self):                                     
+        
+        assert_equals(False, self.__maze.valid)
+        self.__cells[4].add_passages({self.__cells[0]: 5})
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+        print str(self.__maze)
+
+    def test_try_remap_cell(self):
+        
+        assert_equals(False, self.__cells[3].add_passages({self.__cells[0]: sys.maxint}))
+
+    def test_try_remap_route(self):
+        route_list = [self.__cells[0],self.__cells[1],self.__cells[2],self.__cells[3]]
+        assert_equals(True, self.__route.add_cells(route_list))
+        assert_equals(False, self.__route.add_cells(route_list))
+
+    @suite('disabled', reason="Basic Method Test")
+    def test_connected_cells(self):
+
+        print "Testing connected_cells\n"
+
+        for cell in self.__cells[1].connected_cells():
+            print str(cell)
+
+    def test_make_maze(self):
+
+        assert_equals(False, self.__maze.valid)
+        self.__cells[4].add_passages({self.__cells[0]: 5})
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+
+    @suite('disabled', reason="Works as of HW4, but prints over a lot of screen")
+    def test_random_route(self):
+
+        assert_equals(False, self.__maze.valid)
+        self.__cells[4].add_passages({})
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+
+        #makes one of 2 possible routes
+        self.__maze.__route = self.__maze.route_random(self.__cells[1])
+
+        for cell in self.__maze.__route:
+            print str(cell)
+            assert_equals(True, cell.valid)
+
+    def test_first_vs_random_same(self):
+
+        #makes a single possible route from cell 4
+        assert_equals(False, self.__maze.valid)
+        self.__cells.append(Mazes.MazeCell())
+        self.__cells[4].add_passages({self.__cells[5]: 3})
+        self.__cells.append(Mazes.MazeCell())
+        self.__cells[5].add_passages({self.__cells[6]: 3})
+        self.__cells[6].add_passages({})
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+
+        #~%50 chance of different routes each run. 10 loops means 1/2^5 chance of failure
+        for x in xrange(1, 11):
+            #makes the same route from 2 different route methods
+            self.__maze.__route_random = self.__maze.route_random(self.__cells[4])
+            self.__maze.__route_first = self.__maze.route_first(self.__cells[4])
+            assert_equals(self.__maze.__route_random, self.__maze.__route_first)
+
+    def test_first_vs_random_different(self):
+        assert_equals(False, self.__maze.valid)
+        self.__cells[4].add_passages({})
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+
+        #~%50 chance of different routes each run. 10 loops means 1/2^5 chance of failure
+        different = False
+        for x in xrange(1, 11):
+            #makes the same route from 2 different route methods
+            self.__maze.__route_random = self.__maze.route_random(self.__cells[1])
+            self.__maze.__route_first = self.__maze.route_first(self.__cells[1])
+            if self.__maze.__route_random != self.__maze.__route_first:
+                different = True
+
+        assert_equals(True, different)
+
+    def test_invalid_time(self):
+        assert_equals(False, self.__maze.valid)
+        assert_equals(False, self.__cells[4].add_passages({self.__cells[0]: -1}))
+        assert_equals(self.__cells[4].status.INVALID_TIME, self.__cells[4].status.code)
+
+        #try to add real passages after trying to add negative ones
+        assert_equals(True, self.__cells[4].add_passages({self.__cells[0]: 1}))
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+        assert_equals(self.__cells[4].status.OK, self.__cells[4].status.code)
+
+    def test_random_times(self):
+        route_list = [self.__cells[0],self.__cells[1],self.__cells[2],self.__cells[3]]
+        assert_equals(True, self.__route.add_cells(route_list))
+        assert_equals(True, self.__route.valid)
+        assert_between(len(route_list)-1, self.__route.travel_time_random(), self.__route.travel_time())
+
+    def test_cell_not_in_maze(self):
+        #makes a single possible route from cell 4
+        assert_equals(False, self.__maze.valid)
+        self.__cells.append(Mazes.MazeCell())
+        self.__cells[4].add_passages({self.__cells[5]: 3})
+        self.__cells.append(Mazes.MazeCell())
+        self.__cells[5].add_passages({self.__cells[6]: 3})
+        self.__cells[6].add_passages({})
+        self.__maze.add_cells(self.__cells[:-1])
+        assert_equals(True, self.__maze.valid)
+
+        #make sure route_first and route_random return an empty list if a cell is not in maze
+        assert_equals([], self.__maze.route_first(self.__cells[4]))
+        assert_equals([], self.__maze.route_random(self.__cells[4]))
+
+    @suite('disabled', reason="Works as of HW4, but prints over a lot of screen")
+    def test_zgenerate_routes(self):
+        assert_equals(False, self.__maze.valid)
+        self.__cells[4].add_passages({})
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+
+        #tests new generate_route method
+        self.__maze.__route = self.__maze.generate_route(self.__cells[1], self.__maze.choose_random)
+
+        for cell in self.__maze.__route:
+            print str(cell)
+            assert_equals(True, cell.valid)
+
+        #tests new generate_route method
+        self.__maze.__route = self.__maze.generate_route(self.__cells[1], self.__maze.choose_arbitrary)
+
+        for cell in self.__maze.__route:
+            print str(cell)
+            assert_equals(True, cell.valid)
+
+        #tests new generate_route method
+        self.__maze.__route = self.__maze.generate_route(self.__cells[1], self.__maze.choose_greedy)
+
+        for cell in self.__maze.__route:
+            print str(cell)
+            assert_equals(True, cell.valid)
+
+    def test_average_escape_time(self):
+        assert_equals(False, self.__maze.valid)
+        self.__cells[4].add_passages({ self.__cells[0]: 1})
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+
+        assert_equals(sys.maxint, self.__maze.average_exit_time(self.__cells[4] ,self.__maze.choose_random))
+        assert_equals(sys.maxint, self.__maze.average_exit_time(self.__cells[4] ,self.__maze.choose_arbitrary))
+        assert_equals(6, self.__maze.average_exit_time(self.__cells[4] ,self.__maze.choose_greedy))
+
+    def test_average_impossible_route(self):
+        assert_equals(False, self.__maze.valid)
+        self.__cells[4].add_passages({})
+        self.__maze.add_cells(self.__cells)
+        assert_equals(True, self.__maze.valid)
+
+        assert_equals(sys.maxint, self.__maze.average_exit_time(self.__cells[0] ,self.__maze.choose_arbitrary))      
+        assert_equals(sys.maxint, self.__maze.average_exit_time(self.__cells[0] ,self.__maze.choose_random))      
+        assert_equals(sys.maxint, self.__maze.average_exit_time(self.__cells[0] ,self.__maze.choose_greedy))      
+
+    def tearDown(self):
+        del self.__cells[:]
+        del self.__route
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    run()
